a389dec5a70765dfa03e038d2bede82de14064db
Jason Michalski <armooo@armooo.net>
2012-03-01 16:57:57 -0500

    Change compat so tests pass on python2
    We need to hide exec used as a function from python2 to prevent syntax errors.

--- instrumental/compat/__init__.py
+++ instrumental/compat/__init__.py
@@ -8,16 +8,30 @@ if sys.version_info[0] < 3:
     # we'll wrap it up in an eval'd string literal.
     eval(
         compile(
-            "def exec_f(object_, globals_={}, locals_={}):"
-            "    exec object_ in globals_, locals_"
+            """def exec_f(object_, globals_=None, locals_=None):
+                if not globals_ and locals_:
+                   frame = inspect.stack()[1][0]
+                   globals_ = frame.f_globals
+                   locals_ = frame.f_locals
+                elif globals_ and not locals_:
+                    locals_ = globals_
+                exec object_ in globals_, locals_""",
+            '<string>',
+            'exec'
             )
         )
 else:
-    exec_f = exec
-    def execfile(path, globals_=None, locals_=None):
-        if globals_ is None:
-            globals_ = {}
-        if locals_ is None:
-            locals_ = {}
-        with open(path, 'r') as script:
-            return exec(script.read(), globals_, locals_)
+    eval(
+        compile(
+            """exec_f = exec
+                def execfile(path, globals_=None, locals_=None):
+                    if globals_ is None:
+                        globals_ = {}
+                    if locals_ is None:
+                        locals_ = {}
+                    with open(path, 'r') as script:
+                        return exec(script.read(), globals_, locals_)""",
+            '<string>',
+            'exec'
+        )
+    )
