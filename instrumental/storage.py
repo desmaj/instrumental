e9b88d79420519ca5dcffc1366eddb8b656a454e
desmaj <matthew.desmarais@gmail.com>
2013-03-31 23:29:40 -0400

    added result serialization and updated the tests

--- instrumental/storage.py
+++ instrumental/storage.py
@@ -46,32 +46,39 @@ class TextSerializer(object):
         return "\n".join(out) + '\n'
     
     # instrumental-specific constructs
-    def visit_LogicalAnd(self, or_):
-        out = ['LogicalAnd', or_.modulename, or_.label]
-        out.append(self.visit(or_.node))
-        out.append(','.join([self.visit(pragma) for pragma in or_.pragmas]))
-        out.append(';'.join("%r:%s" % (condition, ','.join(results))))
+    def visit_LogicalAnd(self, and_):
+        out = ['LogicalAnd', and_.modulename, and_.label]
+        out.append(self.visit(and_.node))
+        out.append(','.join([self.visit(pragma) for pragma in and_.pragmas]))
+        out.append(';'.join("%r:%s" % (condition, ','.join(results))
+                            for condition, results in and_.conditions.items()))
         return '|'.join(out)
     
     def visit_LogicalOr(self, or_):
         out = ['LogicalOr', or_.modulename, or_.label]
         out.append(self.visit(or_.node))
         out.append(','.join([self.visit(pragma) for pragma in or_.pragmas]))
-        out.append(';'.join("%r:%s" % (condition, ','.join(results))))
+        out.append(';'.join("%r:%s" % (condition, ','.join(results))
+                            for condition, results in or_.conditions.items()))
         return '|'.join(out)
     
     def visit_BooleanDecision(self, decision):
         out = ['BooleanDecision', decision.modulename, decision.label]
         out.append(self.visit(decision.node))
         out.append(','.join([self.visit(pragma) for pragma in decision.pragmas]))
-        out.append(';'.join("%r:%s" % (condition, ','.join(results))))
+        out.append(';'.join("%r:%s" % (condition, ','.join(results))
+                            for condition, results
+                            in decision.conditions.items()))
         return '|'.join(out)
     
     def visit_Comparison(self, comparison):
         out = ['Comparison', comparison.modulename, comparison.label]
         out.append(self.visit(comparison.node))
-        out.append(','.join([self.visit(pragma) for pragma in comparison.pragmas]))
-        out.append(';'.join("%r:%s" % (condition, ','.join(results))))
+        out.append(','.join([self.visit(pragma) 
+                             for pragma in comparison.pragmas]))
+        out.append(';'.join("%r:%s" % (condition, ','.join(results))
+                            for condition, results
+                            in comparison.conditions.items()))
         return '|'.join(out)
     
     # ast nodes
